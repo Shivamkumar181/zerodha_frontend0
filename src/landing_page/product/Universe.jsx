c586d1bd0f6ccca3a3aeec1db0060eb6f582424b
shivam <shivamtech181@gmail.com>
2026-05-24 22:22:44 +0530

    zerodha frontend commit

---
+++ src/landing_page/product/Universe.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+function Universe() {
+    return ( 
+        <div className="container mt-5 ">
+        <div className="row text-center">
+            <h1>The Zerodha Universe</h1>
+            <p>Extend your trading and investment experience even further with our partner platforms</p>
+
+    <div className="col-12">
+      </div>
+       <div className="col-4 p-3">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3 ">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+
+      <div className="col-4 p-3 mt-5">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3 mt-5 ">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3 mt-5">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+
+      <div className="col-4 p-3 mt-5">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3 mt-5 ">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+      <div className="col-4 p-3 mt-5">
+        <img src="media/images/smallcaselogo.png" />
+        <p className="text-small text-muted">Thematic investing platform
+that helps you invest in diversified
+baskets of stocks on ETFs.</p>
+      </div>
+       <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin: "0 auto"}}>Sign up for free</button>
+    </div>  
+    </div>  
+    );
+}
+
+export default Universe;
